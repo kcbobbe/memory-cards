e52c27d1aa2a767aea65556204dbeea4d651bc8b
kcbobbe <kcbobbe@gmail.com>
2018-06-16 11:31:31 -0400

    items from firebase appear on page.

--- memory-cards/src/NewCard.js
+++ memory-cards/src/NewCard.js
@@ -9,6 +9,9 @@ class NewCard extends Component {
     super()
     this.state = {
       gameTitle: '',
+      gameSystem:'',
+      gamePhoto:'',
+      gameMemory:'',
       games: []
 
     }
@@ -24,7 +27,10 @@ class NewCard extends Component {
     e.preventDefault()
     const gamesRef = firebase.database().ref('games')
     const game = {
-      gameTitle: this.state.gameTitle
+      gameTitle: this.state.gameTitle,
+      gameSystem: this.state.gameSystem,
+      gamePhoto: this.state.gamePhoto,
+      gameMemory: this.state.gameMemory
     }
     gamesRef.push(game)
     this.setState({
@@ -40,11 +46,14 @@ class NewCard extends Component {
       for (let game in games) {
         newState.push({
           id: game,
-          gameTitle: games[game].gameTitle
+          gameTitle: games[game].gameTitle,
+          gameSystem: games[game].gameSystem,
+          gamePhoto: games[game].gamePhoto,
+          gameMemory: games[game].gameMemory
         })
       }
       this.setState({
-        game: newState
+        games: newState
       })
     })
   }
@@ -53,21 +62,28 @@ class NewCard extends Component {
     return (
       <div className='new-note-container'>
         <form onSubmit={this.handleSubmit}>
-          <input type='text' name='gameTitle' onChange={this.handleChange} value={this.state.gameTitle} placeholder='Add a favorite video game.' />
+          <input type='text' name='gameTitle' onChange={this.handleChange} value={this.state.gameTitle} placeholder='Title of video game' />
+          <input type='text' name='gameSystem' onChange={this.handleChange} value={this.state.gameSystem} placeholder='Game System' />
+          <input type='text' name='gamePhoto' onChange={this.handleChange} value={this.state.gamePhoto} placeholder='Add a photo URL' />
+          <input type='text' name='gameMemory' onChange={this.handleChange} value={this.state.gameMemomry} placeholder='Add a favorite memory' />
+
           <button>Add Game!</button>
           
         </form>
         <div>
-            <ul>
-              {this.state.games.map((game) => {
-                return (
-                  <li key={game.id}>
-                    <h3>{game.gameTitle}</h3>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          <ul>
+            {this.state.games.map((game) => {
+              return (
+                <li key={game.id}>
+                  <h3>{game.gameTitle}</h3>
+                  <div>{game.gameSystem}</div>
+                  <div><img width='200px' src={game.gamePhoto} /></div>
+                  <div>{game.gameMemory}</div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     )
   }
