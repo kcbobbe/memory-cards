c5486f60644ff8a0bb79bb8e78dd29ed4ced0e2b
kcbobbe <kcbobbe@gmail.com>
2018-08-15 18:49:55 -0400

    buttons do not flip around anymore with the show and hide comments

--- memory-cards/src/GameListCards.js
+++ memory-cards/src/GameListCards.js
@@ -97,22 +97,41 @@ class GameListCards extends React.Component {
           </div>
         </div>
 
-        {!this.state.viewComments && (
+        {!this.state.viewComments && !user && (
           <button className='button-light' onClick={this.viewComments}>Show comments</button>
 
         )}
-        {this.state.viewComments && (
+        {this.state.viewComments && !user && (
           <div>
             <CommentContainer gameId={gameID} />
             <button className='button-light' onClick={this.viewComments}>Hide comments</button>
             {/* <div>Comments:</div> */}
           </div>
         )}
-        {user && (
+
+        {!this.state.viewComments && user && (
+          <div>
+            <button className='button-light' onClick={this.viewComments}>Show comments</button>
+            <Link to={`/comment/${this.props.game.id}`}>
+              <button className='button button-dark comment-button'>Add Comment</button>
+            </Link>
+          </div>
+        )}
+        {this.state.viewComments && user && (
+          <div>
+            <CommentContainer gameId={gameID} />
+            <button className='button-light' onClick={this.viewComments}>Hide comments</button>
+            {/* <div>Comments:</div> */}
+            <Link to={`/comment/${this.props.game.id}`}>
+              <button className='button button-dark comment-button'>Add Comment</button>
+            </Link>
+          </div>
+        )}
+        {/* {user && (
           <Link to={`/comment/${this.props.game.id}`}>
             <button className='button button-dark comment-button'>Add Comment</button>
           </Link>
-        )}
+        )} */}
       </div>
 
     )
